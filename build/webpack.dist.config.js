d6731c7027d3a7de08f9c908f74bc419881abcd6
wangtao <wangt02@zendaimoney.com>
2018-09-14 19:18:32 +0800

    修改打包后的名字

--- build/webpack.dist.config.js
+++ build/webpack.dist.config.js
@@ -6,8 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/dist/',
-    filename: 'vueUpload.js',
-    library: 'vueUpload', // library指定的就是你使用require时的模块名，这里便是require("toastPanel")
+    filename: 'vueAjaxUpload.js',
+    library: 'vueAjaxUpload', // library指定的就是你使用require时的模块名，这里便是require("vueAjaxUpload")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
   },
